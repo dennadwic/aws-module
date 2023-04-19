ee1e90f35b91001cd95e905eed4bcf67ef5d5646
dennadwic <ddc.16.12.95@gmail.com>
2023-04-19 21:54:11 +0700

    Add Bash Script Folder and Provisioner

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -21,9 +21,27 @@ resource "aws_instance" "jenkins" {
   vpc_security_group_ids = var.jenkins_security_group
   subnet_id     = var.jenkins_public_subnet
 
-
   root_block_device {
     volume_size = "10"
+  }
+
+  connection {
+    type = "ssh"
+    user = "ubuntu"
+    private_key = file("D:/StudiDevOps/AWS/SSH/aws-key")
+    host = aws_instance.jenkins.public_ip
+  }
+
+  provisioner "file" {
+    source = "dependency-jenkins.sh"
+    destination = "../bash/dependency-jenkins.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo chmod +x /bash/dependency-jenkins.sh",
+      "./dependency-jenkins.sh",
+    ]
   }
 
   tags = {
